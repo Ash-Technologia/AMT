87a97d43cf7870205419a020882377b670082896
Aayush Singhavi <aayushsinghavi@gmail.com>
2025-08-25 16:47:53 +0530

    New version

--- frontend/src/pages/Policies.jsx
+++ frontend/src/pages/Policies.jsx
@@ -6,12 +6,12 @@ import React from "react";
 const Policies = () => {
   return (
     <div className="page-container">
-      <h1 className="font-bold p-10">Refund, Return and Shipping</h1>
-      <p>Refund: </p>
+      <h1 className="font-bold p-10">Refund, Return and Shipping Policies</h1>
+      <p className="px-10 font-bold py-20">1.Refund Policy: </p>
       <p className="px-20">If approved you will be automatically credited on your original payment method within 7 business day.</p>
- <p>Return: </p>
+ <p className="font-bold px-10 py-20">2.Return Policy: </p>
  <p className="px-20">We do not provide return as of now.</p>
- <p>Shipping: </p>
+ <p className="font-bold px-10 py-20">3.Shipping Policy: </p>
  <p className="px-20">Processing Time: All orders are delivered withing 7-10 business days.</p>
     </div>
   );
